c5cdda297ce211461ccb5e4daef388b44f8c50be
Fabricio <fabriciobs2000@gmail.com>
2020-11-14 23:48:35 -0300

    CRUD completed

--- Projetos/modulo01/index.js
+++ Projetos/modulo01/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 
 const server = express();
+server.use(express.json());
 
 // localhost:3000/teste
 // query params = ?teste=1
@@ -9,11 +10,41 @@ const server = express();
 
 const users = ['Fabricio', 'Lucas', 'Bruno'];
 
+server.get('/users', (req, res) => {
+    return res.json(users);
+})
+
+
 server.get('/users/:index', (req, res) => {
     const { index } = req.params;
 
     return res.json(users[index]);
 });
+
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+
+    res.send();
+})
 
 
 server.listen(3000);
